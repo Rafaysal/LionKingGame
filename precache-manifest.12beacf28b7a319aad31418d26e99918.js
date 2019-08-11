eec794dea987544ca2b51008454796adf265d8de
Rafaysal <rafaysal@hotmail.com>
2019-08-11 00:26:52 -0700

    Updates

---
+++ precache-manifest.12beacf28b7a319aad31418d26e99918.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4346d37b5a98d230c972e3f3b45724f",
+    "url": "/LionKingGame/index.html"
+  },
+  {
+    "revision": "9183142805d9b9c4b1a2",
+    "url": "/LionKingGame/static/css/main.2c6423b9.chunk.css"
+  },
+  {
+    "revision": "198a3da89aa4b4b11caf",
+    "url": "/LionKingGame/static/js/2.cafca0e8.chunk.js"
+  },
+  {
+    "revision": "9183142805d9b9c4b1a2",
+    "url": "/LionKingGame/static/js/main.064546ac.chunk.js"
+  },
+  {
+    "revision": "17b7bd8509d63c5beb5e",
+    "url": "/LionKingGame/static/js/runtime~main.31fc2bde.js"
+  },
+  {
+    "revision": "fa2462edceb930d49cc4b03228fdb847",
+    "url": "/LionKingGame/static/media/(deep-roar)-[AudioTrimmer.com].fa2462ed.mp3"
+  },
+  {
+    "revision": "9b8c988ea67ff27f491f0f323b33548a",
+    "url": "/LionKingGame/static/media/I-just-can-t-wait-to-be-king-the-lion-king-lyrics.9b8c988e.mp3"
+  },
+  {
+    "revision": "546f17a6e5bdebceb3d25d18b4e51d09",
+    "url": "/LionKingGame/static/media/Whoah-NiceOne-Studio.546f17a6.wav"
+  },
+  {
+    "revision": "dbcaa98466e2e4e7d4735141480a978f",
+    "url": "/LionKingGame/static/media/bg.dbcaa984.gif"
+  },
+  {
+    "revision": "71588ab01ea743e5536dccab9d6b4bf1",
+    "url": "/LionKingGame/static/media/boat.71588ab0.png"
+  },
+  {
+    "revision": "3eb5e67bda4ec55b7b63c1dda10a93ec",
+    "url": "/LionKingGame/static/media/circle.3eb5e67b.mp3"
+  },
+  {
+    "revision": "a60a0f204e33e84c817befa48f4706dd",
+    "url": "/LionKingGame/static/media/hyena.a60a0f20.mp3"
+  },
+  {
+    "revision": "ed204fc82a1ea36b6aa9a1e982d55ae3",
+    "url": "/LionKingGame/static/media/hyena.ed204fc8.png"
+  },
+  {
+    "revision": "d4efb3f6c89ac590b641b5fd9190f033",
+    "url": "/LionKingGame/static/media/logo.d4efb3f6.png"
+  },
+  {
+    "revision": "22120872c19cf157f6204759c49d935f",
+    "url": "/LionKingGame/static/media/plank.22120872.png"
+  },
+  {
+    "revision": "3ec112cfb6e9a15fe7affc4a600c1914",
+    "url": "/LionKingGame/static/media/pumba.3ec112cf.wav"
+  },
+  {
+    "revision": "d2f56dff587c5ccb726fd465f530aa88",
+    "url": "/LionKingGame/static/media/pumba.d2f56dff.png"
+  },
+  {
+    "revision": "37ae36cdcde8a72e1547a86b00913d87",
+    "url": "/LionKingGame/static/media/scroll.37ae36cd.png"
+  },
+  {
+    "revision": "3ab21c3a080530e45fd9fea15974a8de",
+    "url": "/LionKingGame/static/media/simba.3ab21c3a.png"
+  },
+  {
+    "revision": "66f9b1b3455169721e970287a4853863",
+    "url": "/LionKingGame/static/media/timon.66f9b1b3.png"
+  }
+]);
